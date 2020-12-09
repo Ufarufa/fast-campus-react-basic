67c2df074ea0d23e06a5c5b50aa8e60d18ca39b7
ufarufa <you@example.com>
2020-12-09 09:21:43 +0900

    immer 사용

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import CreateUser from "./CreateUser";
 import Hello from "./Hello";
 import UserList from "./UserList";
 import useInputs from "./useInputs";
+import produce from "immer";
 
 const initialState = {
   users: [
@@ -30,21 +31,36 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case "CREATE_USER":
-      return {
+      return produce(state, (draft) => {
+        draft.users.push(action.user);
+      });
+
+    /* return {
         users: state.users.concat(action.user),
-      };
+      }; */
     case "TOGGLE_USER":
+      return produce(state, (draft) => {
+        const user = draft.users.find((user) => user.id === action.id);
+        user.active = !user.active;
+      });
+
+    /*
       return {
         ...state,
         users: state.users.map((user) =>
           user.id === action.id ? { ...user, active: !user.active } : user
         ),
       };
+      */
     case "REMOVE_USER":
-      return {
+      return produce(state, (draft) => {
+        const index = draft.users.findIndex((user) => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
+    /*  return {
         ...state,
         users: state.users.filter((user) => user.id !== action.id),
-      };
+      }; */
     default:
       throw new Error("Unhandled action");
   }
